2dcc74017c52b732047a3916e96c9154bf958bc7
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-26 19:33:39 -0400

    ad

---
+++ my-app/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from "next/image"
+
+function Header() {
+    return (
+        <div>
+        <h1>Header</h1>
+
+        {/* Left */}
+        <div>
+            <Image src="links.papareact.com/5me" width={40} height={40}/>
+        </div>
+         {/* Centert */}
+
+          {/* Right*/}
+            
+        </div>
+    )
+}
+
+export default Header
